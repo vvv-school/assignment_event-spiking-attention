fe583b272da73af6167b44ee5c177988f4dbdc03
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2019-03-25 16:07:30 +0100

    Port to RobotTestingFramework 2 and YARP 3.2

--- smoke-test/test.cpp
+++ smoke-test/test.cpp
@@ -5,9 +5,9 @@
 */
 #include <string>
 
-#include <yarp/rtf/TestCase.h>
-#include <rtf/dll/Plugin.h>
-#include <rtf/TestAssert.h>
+#include <yarp/robottestingframework/TestCase.h>
+#include <robottestingframework/dll/Plugin.h>
+#include <robottestingframework/TestAssert.h>
 
 
 #include <yarp/os/all.h>
@@ -15,7 +15,7 @@
 #include <iomanip>
 
 using namespace std;
-using namespace RTF;
+using namespace robottestingframework;
 using namespace yarp::os;
 
 class spikeChecker : public yarp::os::BufferedPort< ev::vBottle >
@@ -93,7 +93,7 @@ public:
 
 
 /**********************************************************************/
-class TestAssignmentEventSpikingAttention : public yarp::rtf::TestCase
+class TestAssignmentEventSpikingAttention : public yarp::robottestingframework::TestCase
 {
 
 private:
@@ -105,7 +105,7 @@ private:
 public:
     /******************************************************************/
     TestAssignmentEventSpikingAttention() :
-        yarp::rtf::TestCase("TestAssignmentEventSpikingAttention")
+        yarp::robottestingframework::TestCase("TestAssignmentEventSpikingAttention")
     {
     }
 
@@ -121,38 +121,38 @@ public:
         //we need to load the data file into yarpdataplayer
         std::string cntlportname = "/playercontroller/rpc";
 
-        RTF_ASSERT_ERROR_IF_FALSE(playercontroller.open(cntlportname),
+        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(playercontroller.open(cntlportname),
                                   "Could not open RPC to yarpdataplayer");
 
-        RTF_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect(cntlportname, "/yarpdataplayer/rpc:i"),
+        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect(cntlportname, "/yarpdataplayer/rpc:i"),
                                   "Could not connect RPC to yarpdataplayer");
 
         //we need to check the output of yarpdataplayer is open and input of spiking model
-        RTF_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect("/zynqGrabber/vBottle:o", "/vSacSup/vBottle:i", "udp"),
+        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect("/zynqGrabber/vBottle:o", "/vSacSup/vBottle:i", "udp"),
                                   "Could not connect yarpdataplayer to assignment module (events)");
 
-        RTF_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect("/icub/torso/state:o", "/vSacSup/torso/state:i", "udp"),
+        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect("/icub/torso/state:o", "/vSacSup/torso/state:i", "udp"),
                                   "Could not connect yarpdataplayer to assignment module (torso)");
 
-        RTF_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect("/icub/head/state:o", "/vSacSup/head/state:i", "udp"),
+        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect("/icub/head/state:o", "/vSacSup/head/state:i", "udp"),
                                   "Could not connect yarpdataplayer to assignment module (head)");
 
-        RTF_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect("/vSacSup/vBottle:o", "/vPreProcess/vBottle:i", "udp"),
+        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect("/vSacSup/vBottle:o", "/vPreProcess/vBottle:i", "udp"),
                                   "Could not connect saccadic suppression to vPreProcess");
 
-        RTF_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect("/vPreProcess/right:o", "/vSpiking/vBottle:i", "udp"),
+        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect("/vPreProcess/right:o", "/vSpiking/vBottle:i", "udp"),
                                   "Could not connect vPreProcess (right) to spiking module");
 
 
         //check we can open our spike checking consumer
-        RTF_ASSERT_ERROR_IF_FALSE(spkchk.open("/spikechecker/vBottle:i"),
+        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(spkchk.open("/spikechecker/vBottle:i"),
                                   "Could not open spike checker");
 
         //the output of spiking model
-        RTF_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect("/vSpiking/vBottle:o", "/spikechecker/vBottle:i", "udp"),
+        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect("/vSpiking/vBottle:o", "/spikechecker/vBottle:i", "udp"),
                                   "Could not connect spiking model to spike checker");
 
-        RTF_TEST_REPORT("Ports successfully open and connected");
+        ROBOTTESTINGFRAMEWORK_TEST_REPORT("Ports successfully open and connected");
 
         return true;
     }
@@ -160,7 +160,7 @@ public:
     /******************************************************************/
     virtual void tearDown()
     {
-        RTF_TEST_REPORT("Closing Clients");
+        ROBOTTESTINGFRAMEWORK_TEST_REPORT("Closing Clients");
         playercontroller.close();
     }
 
@@ -172,51 +172,51 @@ public:
         yarp::os::Bottle cmd, reply;
         cmd.addString("play");
         playercontroller.write(cmd, reply);
-        RTF_ASSERT_ERROR_IF_FALSE(reply.get(0).asString() == "ok", "Did not successfully play the dataset");
+        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(reply.get(0).asString() == "ok", "Did not successfully play the dataset");
 
-        RTF_TEST_REPORT("Playing dataset - please wait till it finishes automatically");
+        ROBOTTESTINGFRAMEWORK_TEST_REPORT("Playing dataset - please wait till it finishes automatically");
         yarp::os::Time::delay(30);
 
         cmd.clear();
         cmd.addString("stop");
         playercontroller.write(cmd, reply);
-        RTF_TEST_REPORT("Stopping dataset - wait for score calculation");
-        RTF_ASSERT_ERROR_IF_FALSE(reply.get(0).asString() == "ok", "Did not successfully stop the dataset");
+        ROBOTTESTINGFRAMEWORK_TEST_REPORT("Stopping dataset - wait for score calculation");
+        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(reply.get(0).asString() == "ok", "Did not successfully stop the dataset");
 
         int score = 0;
-        RTF_TEST_REPORT("Inliers | Outliers");
+        ROBOTTESTINGFRAMEWORK_TEST_REPORT("Inliers | Outliers");
         for(int i = 1; i < 7; i++) {
-            RTF_TEST_REPORT(Asserter::format("Section %d: %d | %d", i, spkchk.inlier[i], spkchk.outlier[i]));
+            ROBOTTESTINGFRAMEWORK_TEST_REPORT(Asserter::format("Section %d: %d | %d", i, spkchk.inlier[i], spkchk.outlier[i]));
             if(spkchk.inlier[i] < 1) {
-                RTF_TEST_REPORT("Missed detection (0)");
+                ROBOTTESTINGFRAMEWORK_TEST_REPORT("Missed detection (0)");
             } else if(spkchk.outlier[i] > 2) {
-                RTF_TEST_REPORT("Too many false detections (0)");
+                ROBOTTESTINGFRAMEWORK_TEST_REPORT("Too many false detections (0)");
             } else {
-                RTF_TEST_REPORT("Good attention (1)");
+                ROBOTTESTINGFRAMEWORK_TEST_REPORT("Good attention (1)");
                 score++;
             }
 
         }
         
-        RTF_TEST_REPORT(Asserter::format("Extra Bonus: %d | %d", spkchk.inlier[0], spkchk.outlier[0]));
+        ROBOTTESTINGFRAMEWORK_TEST_REPORT(Asserter::format("Extra Bonus: %d | %d", spkchk.inlier[0], spkchk.outlier[0]));
         if(spkchk.outlier[0] < 1 && score > 0) {
-            RTF_TEST_REPORT("Good (1)");
+            ROBOTTESTINGFRAMEWORK_TEST_REPORT("Good (1)");
             score++;
         }
 
         //spkchk.printSectionsScores();
         //int score = spkchk.getScore();
-        RTF_TEST_CHECK(score>0, Asserter::format("Total score = %d", score));
+        ROBOTTESTINGFRAMEWORK_TEST_CHECK(score>0, Asserter::format("Total score = %d", score));
 
 //        int inliers = spkchk.getInliers();
 //        int outliers = spkchk.getOutliers();
-//        RTF_TEST_REPORT(Asserter::format("Inliers = %d", inliers));
-//        RTF_TEST_REPORT(Asserter::format("Outliers = %d", outliers));
-//        RTF_ASSERT_ERROR_IF_FALSE(inliers > 3000, "Inlier score too low (3000)");
-//        RTF_ASSERT_ERROR_IF_FALSE(outliers < 500, "Outlier score too high (500)");
+//        ROBOTTESTINGFRAMEWORK_TEST_REPORT(Asserter::format("Inliers = %d", inliers));
+//        ROBOTTESTINGFRAMEWORK_TEST_REPORT(Asserter::format("Outliers = %d", outliers));
+//        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(inliers > 3000, "Inlier score too low (3000)");
+//        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(outliers < 500, "Outlier score too high (500)");
 
 
     }
 };
 
-PREPARE_PLUGIN(TestAssignmentEventSpikingAttention)
+ROBOTTESTINGFRAMEWORK_PREPARE_PLUGIN(TestAssignmentEventSpikingAttention)
